191d8053698ae30b53173e012f7bde061144d852
TheKK <thumbd03803@gmail.com>
2014-04-11 20:00:22 +0800

    Add all file into git

---
+++ CORTEX_M4F_STM32F407ZG-SK/main.c
@@ -0,0 +1,169 @@
+/**
+  ******************************************************************************
+  * @file    Template/main.c 
+  * @author  MCD Application Team
+  * @version V1.0.0
+  * @date    20-September-2013
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2013 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+/** @addtogroup Template
+  * @{
+  */ 
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+
+void RCC_Configuration(void)
+{
+      /* --------------------------- System Clocks Configuration -----------------*/
+      /* USART1 clock enable */
+      RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+      /* GPIOA clock enable */
+      RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+}
+ 
+/**************************************************************************************/
+ 
+void GPIO_Configuration(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+
+    /*-------------------------- GPIO Configuration ----------------------------*/
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    /* Connect USART pins to AF */
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_USART1);   // USART1_TX
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_USART1);  // USART1_RX
+}
+ 
+/**************************************************************************************/
+ 
+void USART1_Configuration(void)
+{
+    USART_InitTypeDef USART_InitStructure;
+
+    /* USARTx configuration ------------------------------------------------------*/
+    /* USARTx configured as follow:
+     *  - BaudRate = 9600 baud
+     *  - Word Length = 8 Bits
+     *  - One Stop Bit
+     *  - No parity
+     *  - Hardware flow control disabled (RTS and CTS signals)
+     *  - Receive and transmit enabled
+     */
+    USART_InitStructure.USART_BaudRate = 115200;
+    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+    USART_InitStructure.USART_StopBits = USART_StopBits_1;
+    USART_InitStructure.USART_Parity = USART_Parity_No;
+    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+    USART_Init(USART1, &USART_InitStructure);
+    USART_Cmd(USART1, ENABLE);
+}
+
+void USART1_puts(char* s)
+{
+    while(*s) {
+        while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+        USART_SendData(USART1, *s);
+        s++;
+    }
+}
+
+/**************************************************************************************/
+/*int main(void)*/
+/*{*/
+	/*RCC_Configuration();*/
+	/*GPIO_Configuration();*/
+	/*USART1_Configuration();*/
+
+	/*USART1_puts("Hello World!\r\n");*/
+	/*USART1_puts("Just for STM32F429I Discovery verify USART1 with USB TTL Cable\r\n");*/
+	/*while(1)*/
+	/*{*/
+		/*while(USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == RESET);*/
+		/*char t = USART_ReceiveData(USART1);*/
+		/*if ((t == '\r')) {*/
+			/*while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);*/
+			/*USART_SendData(USART1, t);*/
+			/*t = '\n';*/
+		/*}*/
+		/*while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);*/
+		/*USART_SendData(USART1, t);*/
+	/*}*/
+
+	/*while(1); // Don't want to exit*/
+/*}*/
+
+void busyloop( uint32_t delay )
+{
+	while( delay )	delay--;
+}
+
+const TickType_t xDelay = 100 / portTICK_PERIOD_MS;
+
+static void UsartTask(void *pvParameters)
+{
+	STM_EVAL_LEDInit( LED4 );
+
+	for( ;; )
+	{
+		vTaskDelay( xDelay );
+		STM_EVAL_LEDToggle( LED4 );
+	}
+}
+
+static void LEDTask( void *pvParameters)
+{
+	STM_EVAL_LEDInit( LED3 );
+
+	for(;;){
+			STM_EVAL_LEDToggle(LED3);
+			vTaskDelay( xDelay );
+	}
+}
+
+//Main Function
+int main(void)
+{
+	//Create Task For USART
+	xTaskCreate(UsartTask, (signed char*)"UsartTask", 128, NULL, tskIDLE_PRIORITY+1, NULL);
+	xTaskCreate(LEDTask, (signed char*)"LEDTask", 128, NULL, tskIDLE_PRIORITY+1, NULL);
+
+	//Call Scheduler
+	vTaskStartScheduler();
+}
---
+++ Makefile
@@ -0,0 +1,121 @@
+PROJECT = usart1
+
+EXECUTABLE = $(PROJECT).elf
+BIN_IMAGE = $(PROJECT).bin
+HEX_IMAGE = $(PROJECT).hex
+
+# set the path to STM32F429I-Discovery firmware package
+STDP ?= ../STM32F429I-Discovery_FW_V1.0.1
+
+# Toolchain configurations
+CROSS_COMPILE ?= arm-none-eabi-
+CC = $(CROSS_COMPILE)gcc
+LD = $(CROSS_COMPILE)ld
+OBJCOPY = $(CROSS_COMPILE)objcopy
+OBJDUMP = $(CROSS_COMPILE)objdump
+SIZE = $(CROSS_COMPILE)size
+
+# Cortex-M4 implements the ARMv7E-M architecture
+CPU = cortex-m4
+CFLAGS = -mcpu=$(CPU) -march=armv7e-m -mtune=cortex-m4
+CFLAGS += -mlittle-endian -mthumb
+# Need study
+CFLAGS += -mfpu=fpv4-sp-d16 -mfloat-abi=softfp -O0
+
+define get_library_path
+    $(shell dirname $(shell $(CC) $(CFLAGS) -print-file-name=$(1)))
+endef
+LDFLAGS += -L $(call get_library_path,libc.a)
+LDFLAGS += -L $(call get_library_path,libgcc.a)
+
+# Basic configurations
+CFLAGS += -g -std=c99
+CFLAGS += -Wall
+
+# Optimizations
+CFLAGS += -g -std=c99 -O3 -ffast-math
+CFLAGS += -ffunction-sections -fdata-sections
+CFLAGS += -Wl,--gc-sections
+CFLAGS += -fno-common
+CFLAGS += --param max-inline-insns-single=1000
+
+# specify STM32F429
+CFLAGS += -DSTM32F429_439xx
+
+# to run from FLASH
+CFLAGS += -DVECT_TAB_FLASH
+LDFLAGS += -T $(PWD)/CORTEX_M4F_STM32F407ZG-SK/stm32f429zi_flash.ld
+
+# STARTUP FILE
+OBJS += $(PWD)/CORTEX_M4F_STM32F407ZG-SK/startup_stm32f429_439xx.o
+
+# STM32F4xx_StdPeriph_Driver
+CFLAGS += -DUSE_STDPERIPH_DRIVER
+CFLAGS += -D"assert_param(expr)=((void)0)"
+
+#My restart
+OBJS += \
+      $(PWD)/CORTEX_M4F_STM32F407ZG-SK/main.o \
+      $(PWD)/CORTEX_M4F_STM32F407ZG-SK/startup/system_stm32f4xx.o \
+      #$(PWD)/CORTEX_M4F_STM32F407ZG-SK/stm32f4xx_it.o \
+
+OBJS += \
+      $(PWD)/croutine.o \
+      $(PWD)/event_groups.o \
+      $(PWD)/list.o \
+      $(PWD)/queue.o \
+      $(PWD)/tasks.o \
+      $(PWD)/timers.o \
+      $(PWD)/portable/GCC/ARM_CM4F/port.o \
+      $(PWD)/portable/MemMang/heap_1.o \
+
+OBJS += \
+    $(PWD)/CORTEX_M4F_STM32F407ZG-SK/Libraries/STM32F4xx_StdPeriph_Driver/src/misc.o \
+    $(PWD)/CORTEX_M4F_STM32F407ZG-SK/Libraries/STM32F4xx_StdPeriph_Driver/src/stm32f4xx_gpio.o \
+    $(PWD)/CORTEX_M4F_STM32F407ZG-SK/Libraries/STM32F4xx_StdPeriph_Driver/src/stm32f4xx_rcc.o \
+    $(PWD)/CORTEX_M4F_STM32F407ZG-SK/Libraries/STM32F4xx_StdPeriph_Driver/src/stm32f4xx_usart.o \
+    $(PWD)/CORTEX_M4F_STM32F407ZG-SK/Libraries/STM32F4xx_StdPeriph_Driver/src/stm32f4xx_syscfg.o \
+    $(PWD)/CORTEX_M4F_STM32F407ZG-SK/Libraries/STM32F4xx_StdPeriph_Driver/src/stm32f4xx_i2c.o \
+    $(PWD)/CORTEX_M4F_STM32F407ZG-SK/Libraries/STM32F4xx_StdPeriph_Driver/src/stm32f4xx_dma.o \
+    $(PWD)/CORTEX_M4F_STM32F407ZG-SK/Libraries/STM32F4xx_StdPeriph_Driver/src/stm32f4xx_exti.o \
+    $(PWD)/Utilities/STM32F429I-Discovery/stm32f429i_discovery.o
+
+CFLAGS += -DUSE_STDPERIPH_DRIVER
+CFLAGS += -I $(PWD)/CORTEX_M4F_STM32F407ZG-SK \
+	  -I $(PWD)/include \
+	  -I $(PWD)/portable/GCC/ARM_CM4F \
+	  -I $(PWD)/CORTEX_M4F_STM32F407ZG-SK/board \
+	  -I $(PWD)/CORTEX_M4F_STM32F407ZG-SK/Libraries/CMSIS/Device/ST/STM32F4xx/Include \
+	  -I $(PWD)/CORTEX_M4F_STM32F407ZG-SK/Libraries/CMSIS/Include \
+	  -I $(PWD)/CORTEX_M4F_STM32F407ZG-SK/Libraries/STM32F4xx_StdPeriph_Driver/inc \
+	  -I $(PWD)/Utilities/STM32F429I-Discovery
+
+all: $(BIN_IMAGE)
+
+$(BIN_IMAGE): $(EXECUTABLE)
+	$(OBJCOPY) -O binary $^ $@
+	$(OBJCOPY) -O ihex $^ $(HEX_IMAGE)
+	$(OBJDUMP) -h -S -D $(EXECUTABLE) > $(PROJECT).lst
+	$(SIZE) $(EXECUTABLE)
+	
+$(EXECUTABLE): $(OBJS)
+	$(LD) -o $@ $(OBJS) \
+		--start-group $(LIBS) --end-group \
+		$(LDFLAGS)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+%.o: %.S
+	$(CC) $(CFLAGS) -c $< -o $@
+
+flash:
+	st-flash write $(BIN_IMAGE) 0x8000000
+
+.PHONY: clean
+clean:
+	rm -rf $(EXECUTABLE)
+	rm -rf $(BIN_IMAGE)
+	rm -rf $(HEX_IMAGE)
+	rm -f $(OBJS)
+	rm -f $(PROJECT).lst
