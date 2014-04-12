96ac8efb9e4e250eb4956831886a00661b4ad080
TheKK <thumbd03803@gmail.com>
2014-04-12 15:51:47 +0800

    ?

--- CORTEX_M4F_STM32F407ZG-SK/main.c
+++ CORTEX_M4F_STM32F407ZG-SK/main.c
@@ -45,8 +45,8 @@
 void RCC_Configuration(void)
 {
       /* --------------------------- System Clocks Configuration -----------------*/
-      /* USART1 clock enable */
-      RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+      /* USART2 clock enable */
+      RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
       /* GPIOA clock enable */
       RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
 }
@@ -66,13 +66,13 @@ void GPIO_Configuration(void)
     GPIO_Init(GPIOA, &GPIO_InitStructure);
 
     /* Connect USART pins to AF */
-    GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_USART1);   // USART1_TX
-    GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_USART1);  // USART1_RX
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_USART2);   // USART2_TX
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_USART2);  // USART2_RX
 }
  
 /**************************************************************************************/
  
-void USART1_Configuration(void)
+void USART2_Configuration(void)
 {
     USART_InitTypeDef USART_InitStructure;
 
@@ -85,21 +85,21 @@ void USART1_Configuration(void)
      *  - Hardware flow control disabled (RTS and CTS signals)
      *  - Receive and transmit enabled
      */
-    USART_InitStructure.USART_BaudRate = 115200;
+    USART_InitStructure.USART_BaudRate = 9600;
     USART_InitStructure.USART_WordLength = USART_WordLength_8b;
     USART_InitStructure.USART_StopBits = USART_StopBits_1;
     USART_InitStructure.USART_Parity = USART_Parity_No;
     USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
     USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-    USART_Init(USART1, &USART_InitStructure);
-    USART_Cmd(USART1, ENABLE);
+    USART_Init(USART2, &USART_InitStructure);
+    USART_Cmd(USART2, ENABLE);
 }
 
-void USART1_puts(char* s)
+void USART2_puts(char* s)
 {
     while(*s) {
-        while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
-        USART_SendData(USART1, *s);
+        while(USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
+        USART_SendData(USART2, *s);
         s++;
     }
 }
@@ -109,21 +109,21 @@ void USART1_puts(char* s)
 /*{*/
 	/*RCC_Configuration();*/
 	/*GPIO_Configuration();*/
-	/*USART1_Configuration();*/
+	/*USART2_Configuration();*/
 
-	/*USART1_puts("Hello World!\r\n");*/
-	/*USART1_puts("Just for STM32F429I Discovery verify USART1 with USB TTL Cable\r\n");*/
+	/*USART2_puts("Hello World!\r\n");*/
+	/*USART2_puts("Just for STM32F429I Discovery verify USART2 with USB TTL Cable\r\n");*/
 	/*while(1)*/
 	/*{*/
-		/*while(USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == RESET);*/
-		/*char t = USART_ReceiveData(USART1);*/
+		/*while(USART_GetFlagStatus(USART2, USART_FLAG_RXNE) == RESET);*/
+		/*char t = USART_ReceiveData(USART2);*/
 		/*if ((t == '\r')) {*/
-			/*while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);*/
-			/*USART_SendData(USART1, t);*/
+			/*while(USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);*/
+			/*USART_SendData(USART2, t);*/
 			/*t = '\n';*/
 		/*}*/
-		/*while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);*/
-		/*USART_SendData(USART1, t);*/
+		/*while(USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);*/
+		/*USART_SendData(USART2, t);*/
 	/*}*/
 
 	/*while(1); // Don't want to exit*/
@@ -157,12 +157,27 @@ static void LEDTask( void *pvParameters)
 	}
 }
 
+static void ButtonTask( void *pvParameters )
+{
+	STM_EVAL_PBInit( BUTTON_USER, BUTTON_MODE_GPIO );
+	STM_EVAL_LEDInit( LED3 );
+	
+	while(1) {
+		if( STM_EVAL_PBGetState( BUTTON_USER ) ){
+			STM_EVAL_LEDOn( LED3 );
+			while( STM_EVAL_PBGetState( BUTTON_USER ) );
+			STM_EVAL_LEDOff( LED3 );
+		}
+	}
+}
+
 //Main Function
 int main(void)
 {
 	//Create Task For USART
 	xTaskCreate(UsartTask, (signed char*)"UsartTask", 128, NULL, tskIDLE_PRIORITY+1, NULL);
-	xTaskCreate(LEDTask, (signed char*)"LEDTask", 128, NULL, tskIDLE_PRIORITY+1, NULL);
+	/*xTaskCreate(LEDTask, (signed char*)"LEDTask", 128, NULL, tskIDLE_PRIORITY+1, NULL);*/
+	xTaskCreate(ButtonTask, (signed char*)"ButtonTask", 128, NULL, tskIDLE_PRIORITY+1, NULL);
 
 	//Call Scheduler
 	vTaskStartScheduler();
